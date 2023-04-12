985035ed97c421bd19a3238f6eaa750549a2287a
Leonardo Santos Silva <1532leleo@gmail.com>
2023-04-12 20:03:18 -0300

    Pagina Jogadores Suica

--- src/suicaJogadores.ts
+++ src/suicaJogadores.ts
@@ -1,1 +1,35 @@
-let setas = document.querySelectorAll('.span1, .span2')
+const controls = document.querySelectorAll('.spanRight, .spanLeft')
+
+let currentItem = 0
+
+let items = document.querySelectorAll('.item')
+
+const maxItens = items.length
+
+controls.forEach(control => {
+    control.addEventListener('click', () => {
+        const isleft = control.classList.contains('spanLeft')
+        if (isleft) {
+            currentItem -= 1
+        } else {
+            currentItem += 1
+        }
+
+        if (currentItem >= maxItens) {
+            currentItem = 0
+        }
+
+        if (currentItem < 0) {
+            currentItem = maxItens - 1
+        }
+
+        items.forEach(item => item.classList.remove('current-item'))
+
+        items[currentItem].scrollIntoView({
+            inline: "center",
+            behavior: "smooth",
+        })
+
+        items[currentItem].classList.add("current-item")
+    })
+})
